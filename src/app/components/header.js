36afa6fc41316857440bb6d1140e28a562462668
imex <official@immersix.xyz>
2024-08-30 03:10:44 +0900

    make header

---
+++ src/app/components/header.js
@@ -0,0 +1,11 @@
+import "./header.css";
+
+export default function Header() {
+    return (
+        <header className=" bg-red-300 w-full h-[150px] " >
+            <div className=" w-full h-full bg-blue-600 text-[10vmin]">
+                I am Header.
+            </div>
+        </header>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      text100per: '180%',
+    },
+
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
